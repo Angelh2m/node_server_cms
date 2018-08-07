8bd2da6d7fe23f0fc465775f210c2eace59e53ad
Angel Hernandez <ahm@Angels-MBP.home>
2018-08-07 14:23:19 -0400

    Replaced pipe as proccess method

--- routes/api/tools.js
+++ routes/api/tools.js
@@ -18,23 +18,53 @@ var sharp = require('sharp');
 var imageUri = 'https://c2.staticflickr.com/2/1723/27650518707_a230a7ccfa_c.jpg';
 
 
+// router.get('/:size', (req, res) => {
+
+//     var readableStream = request(imageUri);
+//     var transformer = sharp()
+//         .resize(Number(req.params.size))
+//         .on('info', function (info) {
+//             console.log('Image height is ' + info.height);
+//         });
+
+//     readableStream.pipe(transformer).pipe(res);
+
+//     // var inputStream = request(imageUri);
+//     // var transformer = sharp().resize(Number(req.params.size));
+//     // inputStream.pipe(transformer).pipe(res);
+
+// });
+
 router.get('/:size', (req, res) => {
+    res.set('Content-Type', 'image/jpg');
 
     var readableStream = request(imageUri);
-    var transformer = sharp()
-        .resize(Number(req.params.size))
-        .on('info', function(info) {
-            console.log('Image height is ' + info.height);
-        });
 
-    readableStream.pipe(transformer).pipe(res);
+    var requestSettings = {
+        url: imageUri,
+        method: 'GET',
+        encoding: null
+    };
+
+    request(requestSettings, function (error, response, body) {
+        res.set('Content-Type', 'image/png');
+        res.send(body);
+    });
+
+    // res.send(readableStream)
+    // var transformer = sharp()
+    //     .resize(Number(req.params.size))
+    //     .on('info', function (info) {
+    //         console.log('Image height is ' + info.height);
+    //     });
+
+    // readableStream.pipe(transformer).pipe(res);
 
     // var inputStream = request(imageUri);
     // var transformer = sharp().resize(Number(req.params.size));
     // inputStream.pipe(transformer).pipe(res);
 
 });
-
 
 
 
